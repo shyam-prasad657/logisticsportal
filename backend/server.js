a6df7042e7c40d968ecba7fdce4fd60dd11ff24e
shyam-prasad657 <shyamsecrets.92@gmail.com>
2025-01-25 00:33:25 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json());
 
 const db = mysql.createConnection({
     host : "localhost",
@@ -11,11 +13,10 @@ const db = mysql.createConnection({
     password : '',
     database : 'complaintportal',
 })
-
+//get() method
 app.get('/', (req, res)=> {
     return res.json("From Backend Side");
 })
-
 app.get('/userdb', (req, res)=> {
     const sql = "SELECT * FROM userdb";
     db.query(sql, (err, data)=>{
@@ -58,6 +59,26 @@ app.get('/vendor', (req, res)=> {
         return res.json(data);
     })
 })
+
+// API Route to Handle Data Submission
+app.post('/submit', (req, res) => {
+    const { name, phone ,id } = req.body;
+  
+    if (!name || !phone || !id) {
+      return res.status(400).json({ error: 'Name and phone number are required.' });
+    }
+  
+    const query = 'INSERT INTO userdb (customerName, customerPhone, accountid) VALUES (?, ?, ?)';
+    db.query(query, [name, phone, id], (err, result) => {
+      if (err) {
+        console.error('Error inserting data:', err);
+        return res.status(500).json({ error: 'Database error.' });
+      }
+  
+      res.status(200).json({ message: 'Data saved successfully!' });
+    });
+  });
+
 app.listen(8081, ()=> {
     console.log("listening...");
 })
